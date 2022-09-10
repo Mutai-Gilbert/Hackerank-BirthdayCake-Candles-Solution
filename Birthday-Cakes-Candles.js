8d227367f58df2302cb26f0579fd6c404e62d6f7
Gilbert Mutai <mutai@MacBook-Pro.local>
2022-09-10 14:52:37 +0300

    Solution

---
+++ Birthday-Cakes-Candles.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let ma= Math.max(...candles);  // finding the maximum in the array candles
+    let sum=0;   // we count the number of maximum in that array
+    for (let i=0; i<candles.length; i++){
+        if (candles[i]==!ma){
+            // we don't return anything in case we don't find the maximum
+        }else if(candles[i]==ma){
+          sum++;  // in case we find the maximum number we increment
+        }
+    }
+    return sum;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
